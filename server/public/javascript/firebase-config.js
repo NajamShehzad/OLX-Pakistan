274abdc080010238d5a9bf902e137613cc7c0400
NajamShehzad <najambutt195@gmail.com>
2018-08-14 15:49:16 +0500

    adding firebase link

---
+++ server/public/javascript/firebase-config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCggl12DJNqEHfe7VRDzQlAG5nEcXb28R4",
+    authDomain: "sylani-fa1f7.firebaseapp.com",
+    databaseURL: "https://sylani-fa1f7.firebaseio.com",
+    projectId: "sylani-fa1f7",
+    storageBucket: "sylani-fa1f7.appspot.com",
+    messagingSenderId: "244619989865"
+};
+firebase.initializeApp(config);
